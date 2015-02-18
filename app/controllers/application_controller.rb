2a0f47ca957afb297926c6a3b90d0a0e5abf5db7
gogojojo <joseph.gochua@gmail.com>
2015-02-18 10:00:00 -0800

    added methods to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def score
+  #action for searching for twitter
+#action for searching on alchemy
+  #redirect to results
+  end 
+  def index
+  end
+  def results
+  end
 end
